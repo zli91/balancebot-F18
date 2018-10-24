6d9a2aacc0d337811cd072f8b20e2f2148a9f853
yueshen <yueshen@umich.edu>
2018-10-24 14:06:47 -0400

    Update mb_structs.h

--- common/mb_structs.h
+++ common/mb_structs.h
@@ -14,6 +14,28 @@ struct mb_state{
     float   right_cmd; //right wheel command [-1..1]
 
     //TODO: Add more variables to this state as needed
+    /* outer loop */
+    // error from previous states
+    float e_phi;
+    float eo1;
+    float eo2;
+
+    // input from previous states
+    float uo1;
+    float uo2;
+
+    /* inner loop */
+    // error from previous states
+    float e_theta;
+    float ei1;
+    float ei2;
+
+    // input from previous states
+    float ui1;
+    float ui2;
+
+    // include setpoint here at state
+    mb_setpoints_t* setpoint;
 };
 
 typedef struct mb_setpoints mb_setpoints_t;
